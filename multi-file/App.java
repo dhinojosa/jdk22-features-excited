a28f39eadc4d879199c6ac9049b93df3696b84d8
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2024-07-14 18:16:08 -0600

    Create multi-file folder for the demos

--- multi-file/App.java
+++
@@ -1,7 +0,0 @@
-class Prog {
-    public static void main(String[] args) { Helper.run(); }
-}
-
-class Helper {
-    static void run() { System.out.println("Hello!"); }
-}
---
+++ multi-file/simple/App.java
@@ -0,0 +1,12 @@
+//All of the following classes must be in the same file
+
+class Prog {
+    public static void main(String[] args) { Helper.run(); }
+}
+
+class Helper {
+    static void run() { System.out.println("Hello!"); }
+}
+
+//Next we will take Prog and put it in Prog.java and Helper and put in Helper.java
+//Will it work? In Java 17? In Java 22+?
