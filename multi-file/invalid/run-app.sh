9824f9d7ec8c2589bc734dd085a3f2fc17a54efb
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2024-09-06 07:06:59 -0400

    added helper scripts

---
+++ multi-file/invalid/run-app.sh
@@ -0,0 +1,1 @@
+java --enable-preview --source=22 Prog.java
--- multi-file/simple/App.java
+++ multi-file/simple/App.java
@@ -1,12 +1,19 @@
 //All of the following classes must be in the same file
 
 class Prog {
-    public static void main(String[] args) { Helper.run(); }
+    public static void main(String[] args) { Helper.run("?"); }
+}
+
+class Zoom {
+    public static void main(String[] args) {
+        Helper.run("!");
+    }
 }
 
 class Helper {
-    static void run() { System.out.println("Hello!"); }
+    static void run(String c) { System.out.println("Hello" + c); }
 }
+
 
 //Next we will take Prog and put it in Prog.java and Helper and put in Helper.java
 //Will it work? In Java 17? In Java 22+?
